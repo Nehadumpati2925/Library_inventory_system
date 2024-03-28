6bb7af94b366f40dd8e2da72ad9a913c5915e141
Nehadumpati2925 <165235416+Nehadumpati2925@users.noreply.github.com>
2024-03-28 11:26:44 +0530

    Update models.py

--- testproject/library/models.py
+++ testproject/library/models.py
@@ -4,19 +4,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-# class MyAccountManger(BaseUserManager):
-#     def create_user(self, email, username, password=None):
-#         if not email:
-#             raise ValueError("User must have email address.")
-#         if not username:
-#             raise ValueError("user must have username.")
-#         user= self.model(
-#             email=self.normalize_email(email),
-#             username=username
-#         )
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
 
 
 class Roles(models.Model):
@@ -25,36 +12,7 @@ class Roles(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=225, null = False)
     status = models.BooleanField(default=True)
-# class CustomUser(AbstractBaseUser):
-#     class Meta():
-#         db_table = 'CustomUser'
-#     id = models.UUIDField(primary_key=True)
-#     email = models.EmailField(verbose_name='email', unique=True, null = False)
-#     password = models.CharField(max_length=225, null = False, default= 'password')
-#     status = models.BooleanField(default=True)
-#     is_active = models.BooleanField(default=True)
-#     firstname = models.CharField(max_length=250)
-#     lastname = models.CharField(max_length=250)
-#     description = models.CharField(max_length=255)
-#     roles_id = models.ForeignKey(Roles, on_delete=models.CASCADE, null = True, blank=True)
-#     date_added = models.DateTimeField(auto_now_add=True)
-#     last_modified = models.DateTimeField(auto_now=True)
-#     is_staff = models.BooleanField(default=True)
 
-#     objects = MyAccountManger()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['username', 'email']
-
-#     def __str__(self):
-#         return self.email
-
-#     def has_perm(self, perm, obj=None):
-#         return self.is_admin
-
-#     def has_module_perms(self, app_label):
-#         return True
-    
 class UserProfile(models.Model): #adding role field with one to one mapping for user table
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roles_id = models.ForeignKey(Roles, on_delete=models.CASCADE, null = True, blank=True)
